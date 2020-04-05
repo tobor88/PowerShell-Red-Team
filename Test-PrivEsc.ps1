3ed5cf450931a4c01dc71fc63e6a2137ed102d87
tobor <rosborne@osbornepro.com>
2020-04-04 23:54:07 -0600

    Update Test-PrivEsc.ps1

--- Test-PrivEsc.ps1
+++ Test-PrivEsc.ps1
@@ -161,4 +161,32 @@ Function Test-PrivEsc {
 
         Get-ChildItem -Path 'C:\Program Files\*','C:\Program Files (x86)\*' | ForEach-Object { Try { Get-Acl -Path $_ -ErrorAction "SilentlyContinue" | Where-Object {($_.Access | Select-Object -ExpandProperty "IdentityReference") -Match "BUILTIN\\Users"} } Catch {$Error[0]}}
 
+#===============================================================================================================
+# CHECK FOR FODHELPER UAC BYPASS
+#===============================================================================================================
+        $Value = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" | Select-Object -Property "ConsentPromptBehaviorAdmin"
+
+        Switch ($Value.ConsentPromptBehaviorAdmin)
+        {
+            0 { $Message = "0 : Elevate without prompting" }
+            1 { $Message = "1 : Prompt for credentials on the secure desktop" }
+            2 { $Message = "2 : Prompt for consent on the secure desktop" }
+            3 { $Message = "3 : Prompt for credentials"}
+            4 { $Message = "4 : Prompt for consent"}
+            5 { $Message = "5 : Prompt for consent for non-Windows binaries"}
+        }  # End Switch
+
+        If (($Value.ConsentPromptBehaviorAdmin -eq 1) -or ($Value.ConsentPromptBehaviorAdmin -eq 2))
+        {
+
+            Write-Host "This device is not vulnerable to the fodhelper UAC bypass method. `nUAC Settings: $Message" -ForegroundColor "Green"
+
+        }  # End If
+        Else 
+        {
+            
+            Write-Host "This device is vulnerable to the fodhelper bypass method. `nCurrent UAC Settings: $Message" -ForegroundColor "Yellow"
+
+        }  # End Else
+
 }  # End Function Test-PrivEsc
