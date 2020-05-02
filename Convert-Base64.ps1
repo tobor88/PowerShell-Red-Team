0c35cc997c0fb3ef96b5464d5eef4264ebfd73f8
tobor <rosborne@osbornepro.com>
2020-05-01 22:24:05 -0600

    Update Convert-Base64.ps1

--- Convert-Base64.ps1
+++ Convert-Base64.ps1
@@ -1,48 +1,55 @@
 <#
-    .SYNOPSIS
+.SYNOPSIS
     Encode or Decode Base64 strings.
 
-    .NOTES
-    Author: Rob Osborne
-    ALias: tobor
-    Contact: rosborne@osbornepro.com
-    https://roberthsoborne.com
 
-    .SYNTAX
-    Convert-Base64 [-Value <string[]>] [{-Decode | -Encode}]
+.SYNTAX
+    Convert-Base64 [-Value] <string[]> {-Decode | -Encode}
 
-    .PARAMETER
-    -Value
+
+.PARAMETERS
+    -Value <String[]>
         Specifies a string to be encoded or decoded with base64.
 
         Enter a string consisting of spaces and special charcters if desired.DESCRIPTION
 
         Required?                    True
-        Position?                    name
+        Position?                    0
         Default value                None
         Accept pipeline input?       True
         Accept wildcard characters?  false
 
-    -Encode
+    -Encode <Switch>
      This switch is used to tell the cmdlet to encode the base64 string
 
-    -Decode
+    -Decode <Switch>
     This switch parameter is used to tell the cmdlet to decode the base64 string
 
-    .INPUTS
-    -Value accepts strings from pipeline.
-    System.String
 
-    .OUTPUTS
-    System.String
-
-    .EXAMPLE
+.EXAMPLE
     -------------------------- EXAMPLE 1 --------------------------
     C:\PS> Convert-Base64 -Value 'Hello World!'' -Encode
     # This example encodes "Hello World into Base64 format.
 
     C:\PS> Convert-Base64 -Value 'SGVsbG8gV29ybGQh' -Decode
     # This example decodes Base64 to a string.
+    
+    
+.NOTES
+    Author: Rob Osborne
+    ALias: tobor
+    Contact: rosborne@osbornepro.com
+    https://roberthsoborne.com
+    
+    
+.INPUTS
+    -Value accepts strings from pipeline.
+    System.String
+
+
+.OUTPUTS
+    System.String
+
 #>
 Function Convert-Base64
 {
